5a5820a6d8b2eafc38e1a8fcff75e3c0c3c538ba
shxr709 <shxr77099@outlook.com>
2025-08-10 10:16:00 +0800

    runoob_test test5.c fin sav

--- runoob_test/test5.c
+++ runoob_test/test5.c
@@ -2,7 +2,33 @@
 
 int main(int argc, char const *argv[])
 {
-    
-    
+    int x, y, z, temp;
+
+    printf("请输入三个整数：");
+    scanf("%d %d %d", &x, &y, &z);
+
+    if (x > y)
+    {
+        temp = x;
+        x = y;
+        y = temp; 
+    }
+
+    if (x > z)
+    {
+        temp = z;
+        z = x;
+        x = temp; 
+    }
+
+    if (y > z)
+    {
+        temp = y;
+        y = z;
+        z = temp; 
+    }
+
+    printf("从小到大排序为：%d %d %d\n", x, y, z);
+
     return 0;
 }
