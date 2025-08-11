c3659e8b730e251c45a79d363ca766f559ca75ec
shxr709 <shxr770099@outlook.com>
2025-08-11 19:19:36 +0800

    vimrc_config sav

---
+++ runoob_test/test6.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    printf("用字符输出字母C！\n");
+    printf(" cccc\n");    
+    printf("c\n");
+    printf("c\n");   
+    printf(" cccc\n");    
+
+    return 0;
+}
