a9ffe0b859fbb2fdf1b6d5acb2973be5de2e2f53
shxr709 <shxr770099@outlook.com>
2025-08-13 20:46:28 +0800

    tidy_up sav

--- test_github/test_github_2.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("test test test\n");
-    return 0;
-}
--- test_github/test_github_3.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-
-int main(int argc, char const *argv[])
-{
-    printf("test github 3");
-    return 0;
-}
